3df20a3aff22864fb84061c74ffc8f4b15f63b2a
Nghia Tran <changeyourcolor@gmail.com>
2016-05-20 15:23:56 +0700

    Integrated with Parse SDK

---
+++ Tek Talk/Tek Talk/ApplicationHelper.swift
@@ -0,0 +1,43 @@
+//
+//  ApplicationHelper.swift
+//  Tek Talk
+//
+//  Created by Nghia Tran Vinh on 5/20/16.
+//  Copyright © 2016 Zyncas. All rights reserved.
+//
+
+import UIKit
+import Parse
+import Bolts
+
+class ApplicationHelper: NSObject {
+    
+    // Init all essential SDK
+    internal static func initAllSDKWithLaunchOption(launchOption : [NSObject : AnyObject]?) {
+        
+        // Parse
+        ApplicationHelper.initParseSDK()
+        
+        // FB
+        ApplicationHelper.initFBSDK()
+    }
+    
+    // Init Parse
+    private static func initParseSDK() {
+        
+        // Initialize Parse.
+        let configuration = ParseClientConfiguration {
+            $0.applicationId = k_PARSE_CLIENT_KEY
+            $0.clientKey = k_PARSE_APPLICATION_ID
+            $0.localDatastoreEnabled = true
+        }
+        
+        Parse.initializeWithConfiguration(configuration)
+        
+    }
+    
+    // Init Facebook SDK
+    private static func initFBSDK() {
+        
+    }
+}
---
+++ Tek Talk/Tek Talk/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  Tek Talk
+//
+//  Created by Nghia Tran Vinh on 5/20/16.
+//  Copyright © 2016 Zyncas. All rights reserved.
+//
+
+import Foundation
+
+
+// PARSE
+let k_PARSE_APPLICATION_ID = "xgxj79iZFoq3c7y1Gif2uhbCoQksiVOl4Xwd43WJ"
+let k_PARSE_CLIENT_KEY = "LcJGIrbZ70W2UIQ6lZodzrAF0XFlP1nFht6wlsbi"
