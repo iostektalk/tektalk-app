406df7e6ff3daed6d527aa80daefe5373049fcaf
Nghia Tran <changeyourcolor@gmail.com>
2016-05-23 10:55:57 +0700

    Finished logic for event screen

--- Tek Talk/Tek Talk/Controller/EventViewController/EventViewModel.swift
+++ Tek Talk/Tek Talk/Controller/EventViewController/EventViewModel.swift
@@ -7,24 +7,35 @@
 //
 
 import UIKit
+import Parse
 
 class EventViewModel: NSObject {
 
+    // Data
+    var arrEvents : [PFObject]? = []
+    weak var tableView : UITableView?
+    
     internal func fetchData() {
-        
+        APIManager.shareInstance.fetchEvent {[weak self] (response : [PFObject]?, error : NSError?)  in
+            self?.arrEvents = response
+            
+            self?.tableView?.reloadData()
+        }
     }
 }
 
 extension EventViewModel :  UITableViewDataSource, UITableViewDelegate {
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+        return (arrEvents?.count)!
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        
+        return 1
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView?.dequeueReusableCellWithIdentifier("CellID", forIndexPath: indexPath)
         
+        return cell!
     }
 }
